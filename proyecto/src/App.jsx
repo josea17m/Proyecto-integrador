31a002f613080d528e6d2fc14e4b8456950fd2c1
josea17m <jamtbarca10@gmail.com>
2022-11-22 19:24:01 -0600

    first commit

---
+++ proyecto/src/App.jsx
@@ -0,0 +1,17 @@
+import SideBar from "./Components/SideBar";
+import TopBar from "./Components/TopBar";
+import MainContent from "./Components/MainContent";
+
+function App() {
+  return (
+    <>
+      <TopBar />
+      <SideBar />
+      <MainContent/>
+      
+      
+    </>
+  );
+}
+
+export default App;
